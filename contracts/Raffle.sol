93dbad578185d70c50fc2ca8c3dd7814f3e5c3e7
thirumurugan7 <thiru82003@gmail.com>
2022-12-12 11:49:31 +0530

    first version of contract is ready

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -9,23 +9,34 @@ contract Raffle {
     //entrance fee for the lottery
     uint256 private immutable i_entranceFee; // should be private cause it involves in payment
 
+    address payable[] private s_players; // should be private cause it involves in payment
+
+    //every time the player wins we need to pay for them
     //prefix i to denote the immutable
     //immutable because it reduces the gas price
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() payable {
+    //to enter the raffle
+
+    function enterRaffle() public payable {
         //require (msg.value > i_entranceFee,"Not enough ETH!")
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
         }
+        //once the player enters the raffle we push them into the s_players array
+        s_players.push(payable(msg.sender));
     }
 
     // function pickRandomWinner(){
     // }
-
+    // to retreive the entrance fee
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
     }
 }
