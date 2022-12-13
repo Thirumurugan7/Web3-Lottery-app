05dfc7d486774d9d35f9307d94821fb415b9dc3a
thirumurugan7 <thiru82003@gmail.com>
2022-12-13 18:26:56 +0530

    deploying script

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,19 +1,28 @@
 const { network, ethers } = require("hardhat")
-const { developmentChains } = require("../helper-hardhat-config")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
+  const chainId = network.config.chainId
+  //const args = [BASE_FEE, GAS_PRICE_LINK]
   let vrfCoordinatorV2Address
   if (developmentChains.includes(network.name)) {
+    //for development network i.e local blockchain
     const vrfCoordinatorV2Mock = await ethers.getContract(
       "VRFCoordinatorV2Mock"
     )
     vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+  } else {
+    //for testnet
+    vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
   }
+
+  const entranceFee = networkConfig[chainId]["entranceFee"]
+  const args = [vrfCoordinatorV2Address, entranceFee] // args for contructor in solidity smart contract
   const raffle = await deploy("Raffle", {
     from: deployer,
-    args: [],
+    args: args,
     log: true,
     waitConfirmation: network.config.blockConfirmation || 1,
   })
