acdefd68f13b14d349ed649530a6db67b736988c
thirumurugan7 <thiru82003@gmail.com>
2022-12-13 19:10:31 +0530

    working on gasLane error

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,29 +1,64 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
+const verify = require("../helper-hardhat-config")
+const dotenv = require("dotenv")
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
   const chainId = network.config.chainId
   //const args = [BASE_FEE, GAS_PRICE_LINK]
-  let vrfCoordinatorV2Address
+  let vrfCoordinatorV2Address, subscriptionId
+
   if (developmentChains.includes(network.name)) {
     //for development network i.e local blockchain
     const vrfCoordinatorV2Mock = await ethers.getContract(
       "VRFCoordinatorV2Mock"
     )
     vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    //creating subscription
+    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+    const transactionReceipt = await transactionResponse.wait(1)
+    subscriptionId = transactionReceipt.events[0].args.subId
+    //Fund  the subscription
+    //for that we need link token on real network
+    await vrfCoordinatorV2Mock.fundSubscription(
+      subscriptionId,
+      VRF_SUB_FUND_AMOUNT
+    )
   } else {
     //for testnet
     vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    subscriptionId = networkConfig[chainId]["subscriptionId"]
   }
 
   const entranceFee = networkConfig[chainId]["entranceFee"]
-  const args = [vrfCoordinatorV2Address, entranceFee] // args for contructor in solidity smart contract
+  const gasLane = networkConfig[chainId]["gasLane"]
+  const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+  const interval = networkConfig[chainId]["interval"]
+  const args = [
+    vrfCoordinatorV2Address,
+    subscriptionId,
+    entranceFee,
+    gasLane,
+    callbackGasLimit,
+    interval,
+  ] // args for contructor in solidity smart contract
+
   const raffle = await deploy("Raffle", {
     from: deployer,
     args: args,
     log: true,
     waitConfirmation: network.config.blockConfirmation || 1,
   })
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log("Verifying...")
+    await verify(raffle.address, args)
+  }
 }
+module.exports.tags = ["all", "raffle"]
